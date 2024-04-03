75eabe61c9ee167cec6026af8736f9c4bf407ec1
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-04-03 15:53:03 +0300

    Persistence Layer Initialize

---
+++ Infrastructure/Eventify_Tutorial_Series.Persistence/DbContexts/EventifyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Eventify_Tutorial_Series.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify_Tutorial_Series.Persistence.DbContexts
+{
+    public class EventifyDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("EventifyDb");
+        }
+    }
+}
---
+++ Infrastructure/Eventify_Tutorial_Series.Persistence/Services/EventService.cs
@@ -0,0 +1,51 @@
+﻿using Eventify_Tutorial_Series.Application.Abstractions.Services;
+using Eventify_Tutorial_Series.Application.DTOs;
+using Eventify_Tutorial_Series.Application.RequestParameters;
+using Eventify_Tutorial_Series.Domain.Entities;
+using Eventify_Tutorial_Series.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify_Tutorial_Series.Persistence.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly EventifyDbContext _context;
+
+        public EventService(EventifyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateEventAsync(CreateEventDTO createEventDTO)
+        {
+            var newEvent = new Event()
+            {
+                Title = createEventDTO.Title,
+                Date = createEventDTO.Date,
+                Location = createEventDTO.Location,
+            };
+
+            await _context.Events.AddAsync(newEvent);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<EventDTO>> GetAllEventsAsync(Pagination pagination)
+        {
+            return await _context.Events.Select(x => new EventDTO()
+            {
+                Title = x.Title,
+                Date = x.Date,
+                Location = x.Location,
+            })
+                .Skip(pagination.PageCount * pagination.ItemCount)
+                .Take(pagination.ItemCount)
+                .ToListAsync();
+        }
+    }
+}
