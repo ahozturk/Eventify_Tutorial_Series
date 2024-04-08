3728a95ef8aab33b3bea2270620943557b2b7909
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-04-08 15:53:52 +0300

    Implementation on Infrastructure

---
+++ Infrastructure/Eventify_Tutorial_Series.Infrastructure/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using Eventify_Tutorial_Series.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify_Tutorial_Series.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        public async Task SaveTextAsync(string text, string path)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(path))
+                    throw new ArgumentNullException(nameof(path));
+
+                await File.WriteAllTextAsync(text, path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while saving the text to file: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Infrastructure/Eventify_Tutorial_Series.Infrastructure/Services/TextService.cs
@@ -0,0 +1,29 @@
+﻿using Eventify_Tutorial_Series.Application.Abstractions.Services;
+using Eventify_Tutorial_Series.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventify_Tutorial_Series.Infrastructure.Services
+{
+    public class TextService : ITextService
+    {
+        public string FormatTextForEvent(IEnumerable<EventDTO> eventItems)
+        {
+            if (eventItems is null)
+                throw new ArgumentNullException(nameof(eventItems));
+
+            StringBuilder stringBuilder = new();
+
+            foreach (var eventItem in eventItems)
+            {
+                if (eventItem is not null)
+                    stringBuilder.AppendLine($"Event: {eventItem.Title} - Location: {eventItem.Location} - Date: {eventItem.Date.ToString("HH:mm - dd/MM/yyyy")}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
